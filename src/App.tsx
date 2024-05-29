37da108fffeef7dd36d4364290a886c32478d350
Merijn Flipsen <mysetery_merijn@outlook.com>
2024-05-29 17:18:26 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TikTakToe from "./components/TikTakToe/TikTak.Toe";
+
+function App() {
+  return (
+    <>
+      <TikTakToe />
+    </>
+  );
+}
+
+export default App;
